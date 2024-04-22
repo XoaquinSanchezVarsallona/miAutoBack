83e7495f1f17931ccbafb8cd8b302a6a0748d664
matias <mchialva@mail.austral.edu.ar>
2024-04-22 20:07:54 -0300

    extras

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -29,7 +29,10 @@ public class Application {
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
         get("/vehicle/family/:familyId", familyController.vehiclesOfFamily);
-        get("/alertas/family/:familyApellido", AlertController.getAlertsOfFamily);
+        get("/alertasss/family/:familyApellido", AlertController.getAlertsOfFamily);
+        post("/alertas/add", AlertController.addAlertToFamily);
+        delete("/alerts/:idAlert", AlertController.deleteAlert);
+
 
         post("/login", userController.login);
         post("/register", userController.register);
@@ -52,9 +55,6 @@ public class Application {
 
         post("/editProfile", userController.editProfile);
         post("/validateToken", userController.validateToken);
-
-        post("/alerts/add", AlertController.addAlertToFamily);
-        delete("/alerts/:idAlert", AlertController.deleteAlert);
 
         post("/editCarProfile" , carController.editCarProfile);
     }}
